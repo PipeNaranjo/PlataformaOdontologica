e6423dd1b974c21118b907abfa67a396c0fb6283
PipeNaranjo <73496689+PipeNaranjo@users.noreply.github.com>
2021-08-02 17:13:45 -0500

    se agrego el loader.io
    Co-Authored-By: Stefannyr3 <70042417+Stefannyr3@users.noreply.github.com>
    Co-Authored-By: Juan2299 <51072327+Juan2299@users.noreply.github.com>

--- SandraOrozcoApp/urls.py
+++ SandraOrozcoApp/urls.py
@@ -1,7 +1,7 @@
-
-
-
+from SandraOrozcoApp.views import loader
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    
+    path('/loaderio-92e7835923e380e0c39b9a40e829aa69/',loader)
 ]
--- SandraOrozcoApp/views.py
+++ SandraOrozcoApp/views.py
@@ -1,4 +1,6 @@
-
+from django.shortcuts import render
 # Create your views here.
 
+def loader(request):
+    return render(request,'loaderio-92e7835923e380e0c39b9a40e829aa69.html')
 
