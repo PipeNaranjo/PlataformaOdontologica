815f2632d2d1092b75030f816f399c35ec85f1bf
PipeNaranjo <73496689+PipeNaranjo@users.noreply.github.com>
2021-08-02 17:25:49 -0500

    Caambio en la URL
    Co-Authored-By: Stefannyr3 <70042417+Stefannyr3@users.noreply.github.com>
    Co-Authored-By: Juan2299 <51072327+Juan2299@users.noreply.github.com>

--- SandraOrozcoApp/urls.py
+++ SandraOrozcoApp/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('/loaderio-92e7835923e380e0c39b9a40e829aa69/',loader)
+    path('loaderio-92e7835923e380e0c39b9a40e829aa69/',loader)
 ]
