99987475814ccd7ad18999810b74e5be044ae167
PipeNaranjo <73496689+PipeNaranjo@users.noreply.github.com>
2021-08-02 17:22:27 -0500

    Update views.py
    Co-Authored-By: Stefannyr3 <70042417+Stefannyr3@users.noreply.github.com>
    Co-Authored-By: Juan2299 <51072327+Juan2299@users.noreply.github.com>

--- SandraOrozcoApp/views.py
+++ SandraOrozcoApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def loader(request):
-    return render(request,'loaderio-92e7835923e380e0c39b9a40e829aa69.html')
+    return render(request,'SandraOrozcoApp/loaderio-92e7835923e380e0c39b9a40e829aa69.html')
 
