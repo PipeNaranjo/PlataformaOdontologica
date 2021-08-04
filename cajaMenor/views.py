3df3b9904539fce2d7183b331a5b963593a799f1
PipeNaranjo <73496689+PipeNaranjo@users.noreply.github.com>
2021-08-04 10:23:50 -0500

    validaciones

--- cajaMenor/views.py
+++ cajaMenor/views.py
@@ -14,22 +14,31 @@ class CajaVista(TemplateView,LoginRequiredMixin):
     models = Evento,Producto
 
     def post(self,request,*args,**kwargs):
-
-        fecha = request.POST.get('fecha')
-        citas = Evento.objects.filter(start=fecha)
-        productos = Producto.objects.filter(fechaRegistro=fecha)
-        error =""
-        if not citas and not productos:
-            error = "No hay ningún registro en la fecha %s" %fecha
-        else:
-            totalP = 0
-            totalC = 0
-            for prd in productos:
-                totalP = totalP + prd.precio
-            for cita in citas:
-                totalC = totalC + int(cita.precio)
+        
+        try:
+            fecha = request.POST.get('fecha')
+            citas = Evento.objects.filter(start=fecha)
+            productos = Producto.objects.filter(fechaRegistro=fecha)
+            error = False
+            if not citas and not productos:
+                error =True
+                mensaje = "No hay ningún registro en la fecha %s" %fecha
+            else:
+                totalP = 0
+                totalC = 0
+                for prd in productos:
+                    totalP = totalP + prd.precio
+                for cita in citas:
+                    totalC = totalC + int(cita.precio)
 
             
             mensaje=str(totalC-totalP)
-
-        return render(request,self.template_name,{'citas':citas,'productos':productos,'error':error,'mensaje':mensaje})
+            return render(request,self.template_name,{'citas':citas,'productos':productos,'error':error,'mensaje':mensaje})
+        except UnboundLocalError:
+            error =True
+            mensaje = "No hay ningún registro en la fecha %s" %fecha
+            return render(request,self.template_name,{'error':error,'mensaje':mensaje})
+        except ValidationError:
+            error =True
+            mensaje = "Formato de fecha incorrecto"
+            return render(request,self.template_name,{'error':error,'mensaje':mensaje})
